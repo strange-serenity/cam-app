b3b1d1cd50bc2c574daa39a3276158fea46593e5
Ivan Kramarenko <kramarenko-ivan@proton.me>
2024-10-15 17:45:04 +0300

    Ready

--- main.js
+++ main.js
@@ -12,12 +12,12 @@ app.on('ready', () => {
 
     mainWindow.loadFile('index.html');
 
-    // Створення контекстного меню
+    // Создание контекстного меню
     const contextMenu = Menu.buildFromTemplate([
         {
-            label: 'Відкрити камеру',
+            label: 'Открыть камеру',
             click: () => {
-                // Відкриття нової сторінки з камерою
+                // Открываем новое окно с камерой
                 const cameraWindow = new BrowserWindow({
                     width: 800,
                     height: 600,
@@ -28,16 +28,23 @@ app.on('ready', () => {
                 });
 
                 cameraWindow.loadFile('camera.html');
+
+                // Обработка получения снимка
+                ipcMain.once('sendImage', (event, imgData) => {
+                    // Передаем изображение в главное окно
+                    mainWindow.webContents.send('receiveImage', imgData);
+                });
             }
         }
     ]);
 
+    // Отображение контекстного меню на главном окне при клике правой кнопкой
     mainWindow.webContents.on('context-menu', (event) => {
         contextMenu.popup();
     });
 });
 
-// Отримання зображення від камери
+// Получение изображения от камеры
 ipcMain.on('sendImage', (event, imgData) => {
     mainWindow.webContents.send('receiveImage', imgData);
 });
